1c2af2b2eb5f42e78ba659eb8e8c37464bc3340a
osama <osamamoahmed242@gmail.com>
2025-12-14 18:03:15 +0200

    feat : Add AppDbContext with entity sets and SQL Server config
    Created AppDbContext in LMS.Context namespace, inheriting from DbContext. Added DbSet properties for Instructor, Student, and Course entities. Configured the context to use a SQL Server database via OnConfiguring.
    
    Make first Migration and link with Server

---
+++ LMS/LMS/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Context
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Entities.Instructor> Instructors { get; set; }
+        public DbSet<Entities.Student> Students { get; set; }
+        public DbSet<Entities.Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=OSOS\\OSAMA;Database=LMS;Trusted_Connection=True;");
+        }
+    }
+}
